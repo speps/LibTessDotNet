73eb4f8addb67b3eaeffcb1c24ba43d40de17d59
Remi Gillig <remigillig@gmail.com>
2015-05-06 19:40:28 +0100

    Add build status and fix build script.

--- Build/BuildCurrentBranch.bat
+++ Build/BuildCurrentBranch.bat
@@ -1,5 +1,7 @@
 @echo off
 setlocal
+set _currentpath=%~dp0
+pushd "%_currentpath%"
 set _msbuildpath=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
 "%_msbuildpath%" ..\LibTessDotNet.sln /t:Clean /p:Configuration=Release
 "%_msbuildpath%" ..\LibTessDotNet.sln /t:Build /p:Configuration=Release
@@ -31,3 +33,5 @@ goto :eof
 :error
 echo Version required
 goto :eof
+
+popd
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-LibTessDotNet
+LibTessDotNet [![Build Status](https://ci.appveyor.com/api/projects/status/32r7s2skrgm9ubva?svg=true)](https://ci.appveyor.com/project/speps/libtessdotnet)
 =============
 
 ### Goal
